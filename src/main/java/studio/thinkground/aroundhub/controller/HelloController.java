461eecbddba8859e660c2c8350fc42acc20e6829
seypak77 <seypak77@gmail.com>
2023-07-24 17:39:30 +0900

    HelloController > Sysout "Hello World" test

--- src/main/java/studio/thinkground/aroundhub/controller/HelloController.java
+++ src/main/java/studio/thinkground/aroundhub/controller/HelloController.java
@@ -1,12 +1,7 @@
 package studio.thinkground.aroundhub.controller;
 
-import org.springframework.web.bind.annotation.*;
-import studio.thinkground.aroundhub.dto.MemberDTO;
-
-import java.util.Map;
-
 public class HelloController {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("Hello World");
     }
 }
