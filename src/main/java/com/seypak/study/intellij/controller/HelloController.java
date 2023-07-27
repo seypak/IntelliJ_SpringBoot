549de0d234bd59acabb3387475ed864479e2cab8
seypak77 <seypak77@gmail.com>
2023-07-27 10:38:30 +0900

    Logback 적용 예제

--- src/main/java/com/seypak/study/intellij/controller/HelloController.java
+++ src/main/java/com/seypak/study/intellij/controller/HelloController.java
@@ -1,13 +1,27 @@
 package com.seypak.study.intellij.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @PostMapping("log-test")
+    public void logTest() {
+        LOGGER.trace("trace Log");
+        LOGGER.debug("debug Log");
+        LOGGER.info("info Log");
+        LOGGER.warn("warn Log");
+        LOGGER.error("error Log");
     }
 }
--- src/main/java/com/seypak/study/intellij/controller/ProductController.java
+++ src/main/java/com/seypak/study/intellij/controller/ProductController.java
@@ -2,7 +2,8 @@ package com.seypak.study.intellij.controller;
 
 import com.seypak.study.intellij.data.dto.ProductDto;
 import com.seypak.study.intellij.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +15,27 @@ import javax.annotation.Resource;
 @RequestMapping("/api/v1/product-api")
 public class ProductController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
+
     @Resource
     private ProductService productService;
 
     //http://localhost:8080/api/v1/product-api/product/{productId}
     @GetMapping(value = "/product/{productId}")
     public ProductDto getProduct(@PathVariable String productId) {
-        return productService.getProduct(productId);
+        long startTime = System.currentTimeMillis();
+        LOGGER.info("[ProductController] perform '{}' of seypak API.", startTime);
+
+        ProductDto productDto = productService.getProduct(productId);
+        // void info(String var1, Object... var2);
+        // var1에 들어가는 argument {}가 순서대로 2,3,4... 파라미터에 데이터가 주입된다.
+        LOGGER.info("[ProductController] Response :: productId ='{}', productName ='{}', productPrice ='{}', productStock ='{}', Response Time = '{}'ms"
+                , productDto.getProductId()
+                , productDto.getProductName()
+                , productDto.getProductPrice()
+                , productDto.getProductStock()
+                , (System.currentTimeMillis()- startTime));
+
+        return productDto;
     }
 }
